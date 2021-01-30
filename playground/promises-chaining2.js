5930c45275730e4a10ff9c02024746e9da928a1b
“fededelrincon” <delrincon@gmail.com>
2021-01-30 12:26:25 -0300

    094- Promise Chaining Challenge

--- playground/promises-chaining2.js
+++ playground/promises-chaining2.js
@@ -1,7 +1,7 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('601426c74ed17129bf97ac72').then((task) => { //findByIdAndDelete es mas rapido xq borra directamente. pero findByIdAndRemove es "mas lento" porque lo busca y lo modifica como vacio, no lo borra como tal, aunque el resultado final es el mismo
+Task.findByIdAndDelete('601426c74ed17129bf97ac72').then((task) => { // findByIdAndRemove va a ser deprecado
     console.log(task) //muestro lo q se va a borrar
     return Task.countDocuments({ completed: false })
 }).then((result) => {
